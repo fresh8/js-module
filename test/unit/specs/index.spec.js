d538b6a0d81478036e167ed955d7b9c06e315099
Sam Stefan <hello@samstefan.co.uk>
2017-02-21 17:55:42 +0000

    Fixing tests for index class.

--- test/unit/specs/index.spec.js
+++ test/unit/specs/index.spec.js
@@ -1,5 +1,4 @@
 import Fresh8 from 'src/index.js';
-import { mockFetchResponse } from '../lib/mock-fetch';
 
 describe('src/index.js', () => {
   describe('Fresh8', () => {
@@ -9,195 +8,143 @@ describe('src/index.js', () => {
       fresh8.remove();
     });
 
+    it('Should bind to "__f8-history-push-state" when "listenOnPushState" is true', done => {
+      const fresh8 = new Fresh8({ instID: '123', listenOnPushState: true });
+      const onHistoryPushStateChangeSpy = sinon.spy();
+      fresh8.reloadAllAds = onHistoryPushStateChangeSpy;
+
+      function handlehistoryPushState () {
+        expect(onHistoryPushStateChangeSpy.called).to.equal(true);
+        fresh8.remove();
+        window.removeEventListener('__f8-history-push-state', handlehistoryPushState);
+        done();
+      }
+
+      window.addEventListener('__f8-history-push-state', handlehistoryPushState);
+      history.pushState({ index: '/context.html' }, 'Home', '/context.html');
+    });
+
     describe('requestAd', () => {
-      it('Should throw error if no options are passed', done => {
+      it('Should throw error if no options are passed', () => {
         const fresh8 = new Fresh8({ instID: '123' });
 
-        fresh8
+        return fresh8
           .requestAd()
           .catch(reason => {
             expect(reason.name).to.equal('invaildeConfig');
             expect(reason.message).to.equal('Missing "slotID"');
             fresh8.remove();
-            done();
           });
       });
 
-      it('Should make a request for the ad with the options passed', done => {
-        const fetchStub = sinon.stub(window, 'fetch');
-        const body = { '54ad56a213fe19232b646047': { instances: [ { data: {}, env: {} } ] } };
-
-        fetchStub.returns(Promise.resolve(mockFetchResponse(body)));
-
+      it('Should make a request for the ad with the options passed', () => {
         const fresh8 = new Fresh8({ instID: '123' });
-        const loadAdStub = sinon.spy(fresh8, '_loadAd');
-        const constructRequestUrlStub = sinon.stub(fresh8, '_constructRequestURL');
+        const loadSpy = sinon.spy();
 
-        constructRequestUrlStub.returns('http://fresh8gaming.com');
-        fresh8.creativeFactoryCache['54ad56a213fe19232b646047'] = () => {};
+        fresh8.Ad.prototype.load = function () {
+          loadSpy();
+          return Promise.resolve();
+        };
 
-        fresh8
+        return fresh8
           .requestAd({ slotID: '312', appendPoint: 'body' })
           .then(() => {
-            expect(constructRequestUrlStub.called).to.equal(true);
-            expect(loadAdStub.called).to.equal(true);
-            expect(fetchStub.called).to.equal(true);
-
-            constructRequestUrlStub.restore();
-            fetchStub.restore();
+            expect(loadSpy.called).to.equal(true);
+            expect(fresh8.ads.length).to.equal(1);
             fresh8.remove();
-            done();
           });
       });
+    });
 
-      it('Should make a request and inject factory script if it\'s not cached', done => {
-        const fetchStub = sinon.stub(window, 'fetch');
-        const body = { '54ad56a213fe19232b646047': { creativePath: 'test.js', instances: [ { data: {}, env: {} } ] } };
-
-        fetchStub.returns(Promise.resolve(mockFetchResponse(body)));
-
+    describe('remove', () => {
+      it('Should call restore the history push state if "listenOnPushState" was set', () => {
         const fresh8 = new Fresh8({ instID: '123' });
-        const constructRequestUrlStub = sinon.stub(fresh8, '_constructRequestURL');
+        const eventSpy = sinon.spy();
 
-        constructRequestUrlStub.returns('http://fresh8gaming.com');
+        fresh8.remove();
+        window.addEventListener('__f8-history-push-state', eventSpy);
+        history.pushState({ index: '/context.html' }, 'Home', '/context.html');
+        window.removeEventListener('__f8-history-push-state', eventSpy);
 
-        // Simulate a network request
-        setTimeout(() => {
-          const scriptLoadedEvent = new Event('__f8-creative-script-loaded');
-          scriptLoadedEvent.creativeFactory = () => {};
-          scriptLoadedEvent.creativeRef = '54ad56a213fe19232b646047';
-          window.dispatchEvent(scriptLoadedEvent);
-        }, 100);
+        expect(eventSpy.called).to.equal(false);
+      });
+    });
 
-        fresh8
-          .requestAd({ slotID: '312', appendPoint: 'body' }).catch(reason => { console.log('reason', reason); })
+    describe('reloadAllAds', () => {
+      it('Should call the reload method on each ad that is currently active', () => {
+        const fresh8 = new Fresh8({ instID: '123' });
+        const reloadSpyOne = sinon.spy();
+        const reloadSpyTwo = sinon.spy();
+
+        fresh8.ads = [
+          { active: true, reload: reloadSpyOne, destroy: () => {} },
+          { active: false, reload: reloadSpyTwo, destroy: () => {} }
+        ];
+
+        return fresh8
+          .reloadAllAds()
           .then(() => {
-            expect(constructRequestUrlStub.called).to.equal(true);
-
-            constructRequestUrlStub.restore();
-            fetchStub.restore();
+            expect(reloadSpyOne.called).to.equal(true);
+            expect(reloadSpyTwo.called).to.equal(false);
             fresh8.remove();
-            done();
           });
       });
     });
 
-    describe('_lookUpAdData', () => {
-      it('Should match on payloads object key', () => {
-        const adData = {
-          payload: {
-            '54ad56a213fe19232b646047': { instances: [ { data: {}, env: {} } ] }
-          },
-          appendPoint: 'body',
-          resolve: () => { },
-          reject: () => { }
-        };
-
+    describe('destroyAllAds', () => {
+      it('Should call the destroy method on each ad that is currently active', () => {
         const fresh8 = new Fresh8({ instID: '123' });
-        fresh8.adDataCache = [ adData ];
+        const destroySpyOne = sinon.spy();
+        const destroySpyTwo = sinon.spy();
 
-        const { ad, index } = fresh8._lookUpAdData('54ad56a213fe19232b646047');
-        expect(ad).to.deep.equal(adData);
-        expect(index).to.deep.equal(0);
-        fresh8.remove();
-      });
+        fresh8.ads = [
+          { active: true, destroy: destroySpyOne },
+          { active: false, destroy: destroySpyTwo }
+        ];
 
-      it('Should return an empty object if payload object key can\'t be matched', () => {
-        const adData = {
-          payload: {
-            '54ad56a213fe19232b646047': { instances: [ { data: {}, env: {} } ] }
-          },
-          appendPoint: 'body',
-          resolve: () => { },
-          reject: () => { }
-        };
-
-        const fresh8 = new Fresh8({ instID: '123' });
-        fresh8.adDataCache = [ adData ];
-
-        const matchedData = fresh8._lookUpAdData('notfound');
-        expect(matchedData).deep.equal({});
-        fresh8.remove();
+        return fresh8
+          .destroyAllAds()
+          .then(() => {
+            expect(destroySpyOne.called).to.equal(true);
+            expect(destroySpyTwo.called).to.equal(false);
+            fresh8.remove();
+          });
       });
     });
 
-    describe('_loadAd', () => {
-      it('Should call the creative factory if found', () => {
-        const factorySpy = sinon.spy();
+    describe('_onCreativeLoaded', () => {
+      it('Should set a factory on eact add that matches the creative ref and ', () => {
         const fresh8 = new Fresh8({ instID: '123' });
-        const lookUpAdDataStub = sinon.stub(fresh8, '_lookUpAdData');
-        const adData = {
-          payload: {
-            '54ad56a213fe19232b646047': { instances: [ { data: {}, env: {} } ] }
+        const creativeFactorySpy = sinon.spy();
+        const setCreativeFactorySpyOne = sinon.spy();
+        const callCreativeFactorySpyOne = sinon.spy();
+        const setCreativeFactorySpyTwo = sinon.spy();
+        const callCreativeFactorySpyTwo = sinon.spy();
+
+        fresh8.ads = [
+          {
+            active: true,
+            awaitingFactory: true,
+            creativeRef: '1',
+            destroy: () => {},
+            _setCreativeFactory: setCreativeFactorySpyOne,
+            _callCreativeFactory: callCreativeFactorySpyOne
           },
-          appendPoint: 'body',
-          resolve: () => { },
-          reject: () => { }
-        };
+          {
+            active: true,
+            awaitingFactory: false,
+            creativeRef: '1',
+            destroy: () => {},
+            _setCreativeFactory: setCreativeFactorySpyTwo,
+            _callCreativeFactory: callCreativeFactorySpyTwo
+          }
+        ];
 
-        lookUpAdDataStub.returns({ ad: adData, index: 0 });
-        fresh8.adDataCache = [ adData ];
-        fresh8._loadAd(factorySpy, '54ad56a213fe19232b646047');
-        expect(factorySpy.called).to.equal(true);
+        fresh8._onCreativeLoaded({ creativeFactory: creativeFactorySpy, creativeRef: '1' });
 
-        lookUpAdDataStub.restore();
-        fresh8.remove();
-      });
+        expect(setCreativeFactorySpyOne.called).to.equal(true);
+        expect(callCreativeFactorySpyOne.called).to.equal(true);
 
-      it('Should\'t call the creative factory not found', () => {
-        const factorySpy = sinon.spy();
-        const fresh8 = new Fresh8({ instID: '123' });
-        const lookUpAdDataStub = sinon.stub(fresh8, '_lookUpAdData');
-        const adData = null;
-
-        lookUpAdDataStub.returns({ ad: adData, index: 0 });
-        fresh8.adDataCache = [ adData ];
-        fresh8._loadAd(factorySpy, '54ad56a213fe19232b646047');
-        expect(factorySpy.called).to.equal(false);
-
-        lookUpAdDataStub.restore();
-        fresh8.remove();
-      });
-    });
-
-    describe('_addEventLisnter', () => {
-      it('Should bind "__f8-creative-script-loaded" to window', () => {
-        const factorySpy = sinon.spy();
-        const fresh8 = new Fresh8({ instID: '123' });
-        const loadAdStub = sinon.stub(fresh8, '_loadAd');
-        loadAdStub.returns();
-
-        const scriptLoadedEvent = new Event('__f8-creative-script-loaded');
-        scriptLoadedEvent.creativeFactory = factorySpy;
-        scriptLoadedEvent.creativeRef = '54ad56a213fe19232b646047';
-        window.dispatchEvent(scriptLoadedEvent);
-
-        expect(fresh8.creativeFactoryCache).to.deep.equal({ '54ad56a213fe19232b646047': factorySpy });
-
-        loadAdStub.restore();
-        fresh8.remove();
-      });
-    });
-
-    describe('_constructRequestURL', () => {
-      it('Should return constructed URL', () => {
-        const fresh8 = new Fresh8({ instID: '123' });
-        const URL = fresh8._constructRequestURL();
-        expect(URL).to.equal('https://fresh8.co/123/raw?');
-        fresh8.remove();
-      });
-
-      it('Should return constructed URL when options passed', () => {
-        const fresh8 = new Fresh8({ instID: '123' });
-        const options = {
-          competitorIds: ['1443', '5677'],
-          competitors: ['preston'],
-          competitionIds: ['125454'],
-          competitions: ['Premier League']
-        };
-
-        const URL = fresh8._constructRequestURL(options);
-        expect(URL).to.equal('https://fresh8.co/123/raw?competitorIds=1443,5677&competitors=preston&competitionIds=125454&competitions=Premier%20League&');
         fresh8.remove();
       });
     });
