4c188b427f81092a38ec77d011e919dfaf0d90e9
Sam Stefan <hello@samstefan.co.uk>
2017-02-21 17:56:11 +0000

    Adding tests for history push state.

--- test/unit/specs/polyfill.spec.js
+++ test/unit/specs/polyfill.spec.js
@@ -3,29 +3,31 @@ import { customEvent, PollyfillHistoryPushState } from 'src/polyfill.js';
 describe('src/index.js', () => {
   describe('PollyfillHistoryPushState', () => {
     describe('restore', () => {
-      it('Should remove the custome event on restore', () => {
+      it('Should remove the custom event on restore', () => {
         const pollyfillHistoryPushState = PollyfillHistoryPushState();
         pollyfillHistoryPushState.fill();
         pollyfillHistoryPushState.restore();
         const eventSpy = sinon.spy();
 
-        window.addEventListener('__f8-creative-script-loaded', eventSpy);
+        window.addEventListener('__f8-history-push-state', eventSpy);
         history.pushState({ index: '/context.html' }, 'Home', '/context.html');
+        window.removeEventListener('__f8-history-push-state', eventSpy);
 
         expect(eventSpy.called).to.equal(false);
       });
     });
 
     describe('fill', () => {
-      it('Should add a custome event when the history pushState method is called', done => {
+      it('Should add a custome event when the history pushState method is called', () => {
         const pollyfillHistoryPushState = PollyfillHistoryPushState();
         pollyfillHistoryPushState.fill();
         const eventSpy = sinon.spy();
 
-        window.addEventListener('__f8-creative-script-loaded', eventSpy);
+        window.addEventListener('__f8-history-push-state', eventSpy);
         history.pushState({ index: '/context.html' }, 'Home', '/context.html');
 
         expect(eventSpy.called).to.equal(true);
+        window.removeEventListener('__f8-history-push-state', eventSpy);
       });
     });
   });
